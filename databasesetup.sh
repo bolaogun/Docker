58a6f14671ecef2bce2d1e005b5c9d14e6d2fd8f
bolaogun <a.ao@hotmail.co.uk>
2017-03-20 17:11:03 +0000

    Update databasesetup.sh
    Removed redundant Comments from script

--- databasesetup.sh
+++ databasesetup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-#/usr/bin/mysqld_safe
 service mysql start
 mysql -uroot -e "CREATE DATABASE  ${database};"
 mysql -uroot -e "CREATE USER '${username}'@'localhost' IDENTIFIED BY '${password}';"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON ${database}.* TO '${username}'@'localhost' WITH GRANT OPTION;"
-#mysqladmin -uroot shutdown
 service mysql stop
